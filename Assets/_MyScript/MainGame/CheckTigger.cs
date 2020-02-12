ec4d9a5ce5ae42cd0e0c2409ba5ec23d8538a3a6
Maisang <sarusasiri@gmail.com>
2020-02-12 14:03:14 +0700

    camera switch

---
+++ Assets/_MyScript/MainGame/CheckTigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public bool _stay = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+           
+            //cM.view[1].gameObject.SetActive(true);
+            //cM.view[0].gameObject.SetActive(false);
+            _stay = true;
+        }
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            //cM.view[0].gameObject.SetActive(true);
+            //cM.view[1].gameObject.SetActive(false);
+            _stay = false;
+        }
+    }
+}
--- Assets/_MyScript/MainGame/SwitchCM.cs
+++ Assets/_MyScript/MainGame/SwitchCM.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SwitchCM : MonoBehaviour
 {
+    //public static SwitchCM _SwitchCM;
     // Start is called before the first frame update
     public Transform[] view;
     public float traitionSpeed;
-   private Transform currentView;
+    public Transform currentView;
+    public CheckTigger checkTigger;
 
     //public GameObject player;
     //private Vector3 offset;//distance between the player's position and camera's position.
@@ -21,15 +23,16 @@ public class SwitchCM : MonoBehaviour
         //gameOjectTaget2.SetActive(false);
 
         //offset = transform.position - player.transform.position;
-        view[0].gameObject.SetActive(true);
-        view[1].gameObject.SetActive(false);
+        currentView = view[0].GetComponent<Transform>();
+        //view[0].gameObject.SetActive(true);
+        //view[1].gameObject.SetActive(false);
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         //if (Input.GetKeyDown(KeyCode.Mouse1))
         //{
         //    currentView = view[1];
@@ -45,26 +48,25 @@ public class SwitchCM : MonoBehaviour
         //    currentView = view[2];
         //    switchInput = true;
         //}
-        //switch (count)
-        //{
-        //    case -1:
-        //        currentView = view[1];
-        //        switchInput = true;
-        //        break;
 
-        //    case 0:
-        //        currentView = view[0];
-        //        switchInput = false;
-        //        break;
+        switch (checkTigger._stay)
+        {
+            case true:
+                currentView = view[1];
 
-        //    case 1:
+                break;
 
-        //        currentView = view[2];
-        //        switchInput = false;
-        //        break;
+            case false:
+                currentView = view[0];
+
+                break;
 
 
-        //}
+
+
+        }
+
+
 
 
         //ControlCamera();
@@ -73,33 +75,33 @@ public class SwitchCM : MonoBehaviour
 
     private void LateUpdate()
     {
-        //////transform.position = player.transform.position + offset;
-        //transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * traitionSpeed);
-        //Vector3 currentAngle = new Vector3(
-        //    Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * traitionSpeed),
-        //    Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * traitionSpeed),
-        //    Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * traitionSpeed));
+        ////transform.position = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * traitionSpeed);
+        Vector3 currentAngle = new Vector3(
+            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * traitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * traitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * traitionSpeed));
 
-        //transform.eulerAngles = currentAngle;
+        transform.eulerAngles = currentAngle;
 
 
 
 
     }
 
-    public void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            view[1].gameObject.SetActive(true);
-            view[0].gameObject.SetActive(false);
-        }
+    //public void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        view[1].gameObject.SetActive(true);
+    //        view[0].gameObject.SetActive(false);
+    //    }
        
-    }
+    //}
 
-    public void OnTriggerExit(Collider other)
-    {
-        view[0].gameObject.SetActive(true);
-        view[1].gameObject.SetActive(false);
-    }
+    //public void OnTriggerExit(Collider other)
+    //{
+    //    view[0].gameObject.SetActive(true);
+    //    view[1].gameObject.SetActive(false);
+    //}
 }
