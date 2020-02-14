1183d02cafdf771a3fbf01786e838bfadd17f2a4
Maisang <sarusasiri@gmail.com>
2020-02-15 02:33:58 +0700

    edit timeline

--- Assets/_MyScript/MainGame/Player/Interact.cs
+++ Assets/_MyScript/MainGame/Player/Interact.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Interact : MonoBehaviour
 {
     public GameObject texFist;
+    bool isShowText;
     public GameObject texSceond;
     // Start is called before the first frame update
     void Start()
     {
-        
+        texFist.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isShowText)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                StartCoroutine(wait());
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -23,6 +30,30 @@ public class Interact : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("press E");
+            isShowText = true;
+            
         }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        Debug.Log("press E");
+    //    }
+    //}
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Out");
+        texFist.SetActive(false);
+    }
+
+    IEnumerator wait()
+    {
+        texFist.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        texFist.SetActive(false);
+        isShowText = false;
+
     }
 }
