81b54a8c9e4c3f56aac3da69e0b3d60ffeb3430e
Maisang <sarusasiri@gmail.com>
2020-03-05 13:39:31 +0700

    fix wood crack

---
+++ Assets/_MyScript/MainGame/Even_Light.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Even_Light : MonoBehaviour
+{
+    public GameObject scen_1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scen_1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            scen_1.SetActive(true);
+            gameObject.GetComponent<Collider>().enabled = false;
+            SoundManager.soundManager.audioS.volume = 1f;
+            SoundManager.soundManager.PlaySound(soundInGame.hit_sound);
+           
+        }
+    }
+
+    
+}
---
+++ Assets/_MyScript/MainGame/EventLogFall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventLogFall : MonoBehaviour
+{
+    public Rigidbody[] rigi;
+    // Start is called before the first frame update
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        foreach(Rigidbody g in rigi)
+        {
+            g.useGravity = true;
+            g.isKinematic = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+}
