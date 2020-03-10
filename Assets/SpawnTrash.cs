5f1b113dc39b4244dd4357006bce3d922f5705db
Maisang <sarusasiri@gmail.com>
2020-03-10 22:02:50 +0700

    make room trash

---
+++ Assets/SpawnTrash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrash : MonoBehaviour
+{
+    public GameObject[] prefeb;
+   
+    public int prefeb_num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int prefeb_num = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prefeb_num = Random.Range(0, 5);
+        Instantiate(prefeb[prefeb_num], transform.position, transform.rotation);
+    }
+}
