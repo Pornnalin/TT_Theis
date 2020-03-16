1ee0cfec7f5d1b0f84664ca2f49d957cd2757a49
Maisang <sarusasiri@gmail.com>
2020-03-16 16:30:25 +0700

    add eletic pole

--- Assets/_MyScript/MainGame/EventPlaform.cs
+++ Assets/_MyScript/MainGame/EventPlaform.cs
@@ -10,6 +10,15 @@ public class EventPlaform : MonoBehaviour
     public GameObject switchButton;
     public GameObject lightButtnOn;
     public GameObject lightButtOff;
+    public GameObject thisLightOn;
+    public GameObject thisLightOff;
+
+    public GameObject[] pole;
+    public GameObject[] paticelEle;
+    public Material changeMat;
+    //public Renderer renderer;
+    bool isOff;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +30,31 @@ public class EventPlaform : MonoBehaviour
         lightButtOff.SetActive(true);
         lightButtnOn.SetActive(false);
         switchButton.gameObject.GetComponent<Collider>().enabled = false;
-        
+        thisLightOn.SetActive(false);
+        thisLightOff.SetActive(true);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isOff)
+        {
+            thisLightOn.SetActive(true);
+            thisLightOff.SetActive(false);
+            isSwtichOn = true;
+            foreach(GameObject game in paticelEle)
+            {
+                game.SetActive(true);
+            }
+            foreach (GameObject po in pole)
+            {
+                po.GetComponent<Renderer>().material = changeMat;
+
+            }
+            //gameObject.GetComponent<Collider>().enabled = false;
+        }
+
         if (isSwtichOn)
         {
             lightButtOff.SetActive(false);
@@ -36,14 +64,42 @@ public class EventPlaform : MonoBehaviour
             wall[1].SetActive(true);
             switchButton.gameObject.GetComponent<Collider>().enabled = true;
         }
+
+      
     }
 
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        if (isOff) 
+    //        {
+    //            thisLightOn.SetActive(true);
+    //            thisLightOff.SetActive(false);
+    //            isSwtichOn = true;
+    //            gameObject.GetComponent<Collider>().enabled = false;
+    //        }
+
+
+    //    }
+    //}
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            isSwtichOn = true;
-            gameObject.GetComponent<Collider>().enabled = false;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                thisLightOn.SetActive(true);
+                thisLightOff.SetActive(false);
+                isOff = true;
+                gameObject.GetComponent<Collider>().enabled = false;
+
+                //Debug.Log(thisLightOff.activeSelf);
+
+            }
+
+
 
         }
     }
