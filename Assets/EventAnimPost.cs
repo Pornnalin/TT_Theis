7287c0f42748680949cc324fb98ded37761b1f10
Maisang <sarusasiri@gmail.com>
2020-03-28 00:59:22 +0700

    cutscene

---
+++ Assets/EventAnimPost.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventAnimPost : MonoBehaviour
+{
+    Animator anim;
+    public GameObject blockWay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        blockWay.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("Play", true);
+            blockWay.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/EventriggerDie.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EventriggerDie : MonoBehaviour
+{
+    public GameObject evenDie;
+    // Start is called before the first frame update
+    void Start()
+    {
+        evenDie.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //MainPlayerController.instance.anim.SetFloat("Speed", 0);
+            //GameManager._gameEnd = true;
+            //evenDie.SetActive(true);
+            //gameObject.GetComponent<Collider>().enabled = false;
+            StartCoroutine(wait());
+
+        }
+    }
+
+    IEnumerator wait()
+    {
+        MainPlayerController.instance.anim.SetFloat("Speed", 0);
+        GameManager._gameEnd = true;
+        evenDie.SetActive(true);
+        gameObject.GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(8f);
+        SceneManager.LoadScene("TestLevel_4");
+
+    }
+}
