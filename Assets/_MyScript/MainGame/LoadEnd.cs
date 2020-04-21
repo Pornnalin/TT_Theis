6411f9b204a4587cde3612407dfdc6bf3b029c4d
Maisang <sarusasiri@gmail.com>
2020-04-22 01:16:01 +0700

    edit endcredit

---
+++ Assets/_MyScript/MainGame/LoadEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("EndCredit");
+        //CheckPointControl.checkPointControl.lastCheckPos = new Vector3(-51.8f, 0.267f, -1.73f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
